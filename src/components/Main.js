3233f7107225286941dd20644b4302d33ef4c225
TitanV <a01098930999@gmail.com>
2021-05-25 18:28:05 +0900

    update - many

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,8 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import axios from 'axios';
-import { Slide } from 'react-slideshow-image';
 import { Spinner } from "react-loading-io";
 import * as config from '../config';
 import reseticon from '../public/reset.png';
-import fantasy from '../public/fantasy.png';
-import modernfantasy from '../public/modernfantasy.png';
-import martial from '../public/martial.png';
-import mystery from '../public/mystery.png';
-import romance from '../public/romance.png';
-import romancefantasy from '../public/romancefantasy.png';
-import 'react-slideshow-image/dist/styles.css'
 
 import '../style/Main.css';
 
@@ -21,30 +13,30 @@ class Main extends Component {
     this.state = { 
       input: '',
       output: '',
-      next: '',
       outputBeforeTlanslate: '',
       outputAfterTlanslate: '',
-      temp: '',
       loading: false,
       options: ['판타지','현판','무협','미스터리','로판'],
       selectOptions: '판타지',
-      mainChar: ''
+      Main_character: '',
+      Place: '',
+      Time: '',
+      Main_Events: '',
+      Material: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.requestcontents = this.requestcontents.bind(this);
     this.onSelect = this.onSelect.bind(this);
     this.resetData = this.resetData.bind(this);
-    this.handleChangeN = this.handleChangeN.bind(this);
-    this.requestNextContents = this.requestNextContents.bind(this);
   }
 
-  handleChange(event){
-    this.setState({input: event.target.value});
+  handleChange(e){
+    this.setState({[e.target.name]: e.target.value});
   }
 
-  handleChangeN(event){
-    this.setState({next: event.target.value});
+  onSelect(e){
+    this.setState({ selectOptions: e.target.value});
   }
 
   requestcontents(){
@@ -52,15 +44,17 @@ class Main extends Component {
     this.setState({loading: true});
 
     axios.post(`${config.SERVER_URL}/complation`, 
-    { input: this.state.input,
-      category: this.state.selectOptions }, 
+    { selectOptions: this.state.selectOptions,
+      Main_character: this.state.Main_character,
+      Place: this.state.Place,
+      Time: this.state.Time,
+      Main_Events: this.state.Main_Events,
+      Material: this.state.Material }, 
     { headers: {authentication: localStorage.getItem('token')},
     timeout: 10000 })
     .then((response) => {
-      this.setState({ mainChar: this.state.input});
       this.setState({ outputAfterTlanslate: this.state.outputAfterTlanslate + response.data.output});
       this.setState({ outputBeforeTlanslate: this.state.outputBeforeTlanslate + response.data.outputBeforeTlanslate});
-      this.setState({ temp: response.data.outputBeforeTlanslate});
       this.setState({ output: this.state.outputAfterTlanslate+ '\n\n원본\n'+ this.state.outputBeforeTlanslate});
       this.setState({loading: false});
     })
@@ -74,77 +68,41 @@ class Main extends Component {
 
   }
 
-  onSelect(event){
-    this.setState({ selectOptions: event.target.value});
-  }
 
   resetData(){
     this.setState({next: ''});
     this.setState({input: ''});
     this.setState({output: ''});
-    this.setState({mainChar: ''});
     this.setState({outputBeforeTlanslate: ''});
     this.setState({outputAfterTlanslate: ''});
   }
 
 
-  requestNextContents(next){
-    if (localStorage.getItem('token') !== undefined) {
-    this.setState({loading: true});
-    console.log(this.state.next);
-
-
-    axios.post(`${config.SERVER_URL}/next`, 
-    { mainCharacter: this.state.mainChar,
-      category: this.state.selectOptions,
-      story: this.state.temp,
-      action: this.state.next }, 
-    { headers: {authentication: localStorage.getItem('token')},
-    timeout: 10000 })
-    .then((response) => {
-      this.setState({ outputAfterTlanslate: this.state.outputAfterTlanslate + response.data.output});
-      this.setState({ outputBeforeTlanslate: this.state.outputBeforeTlanslate + response.data.outputBeforeTlanslate});
-      this.setState({ output: this.state.outputAfterTlanslate+ '\n\n원본\n'+ this.state.outputBeforeTlanslate});
-      this.setState({loading: false});
-    })
-    .catch((error) => {
-      this.setState({loading: false});
-      this.setState({ output: '토큰이 부족합니다!'});
-    })
-    }else{
-      this.setState({output: '로그인 후 다시 시도해 주세요!'});
-    }
-  }
-
   render() {
-    const images = [
-      mystery,
-      martial,
-      fantasy,
-      modernfantasy,
-      romance,
-      romancefantasy
-    ];
     return (
-      <div class="main">
-
-        {this.state.loading ? 
-          <div class="loading">
-            <Spinner size='8px' color='#3b2479'/>
-          </div>
-        : null}
-        <div>
-          <div class="input">
-                  <select className='dropdowncategory' onChange={this.onSelect} >
-          <option value={this.state.options[0]}>{this.state.options[0]}</option>
-          <option value={this.state.options[1]}>{this.state.options[1]}</option>
-          <option value={this.state.options[2]}>{this.state.options[2]}</option>
-          <option value={this.state.options[3]}>{this.state.options[3]}</option>
-          <option value={this.state.options[4]}>{this.state.options[4]}</option>
+      <Fragment>
+      <div class="sub">
+        <div class="input">
+          <select className='dropdowncategory' onChange={this.onSelect} >
+            <option value={this.state.options[0]}>{this.state.options[0]}</option>
+            <option value={this.state.options[1]}>{this.state.options[1]}</option>
+            <option value={this.state.options[2]}>{this.state.options[2]}</option>
+            <option value={this.state.options[3]}>{this.state.options[3]}</option>
+            <option value={this.state.options[4]}>{this.state.options[4]}</option>
         </select>
-          <input class="input_text" value={this.state.input} onChange={this.handleChange} placeholder="당신의 주인공을 입력하세요!"></input>
-          <button class="start" onClick = {this.requestcontents}>Start!</button>
-          </div>
+        </div>
+        <input class="sub_input_text" value={this.state.Main_character} onChange={this.handleChange} name='Main_character' placeholder="주요 인물"></input>
+        <input class="sub_input_text" value={this.state.Place} onChange={this.handleChange} name='Place' placeholder="장소"></input>
+        <input class="sub_input_text" value={this.state.Time} onChange={this.handleChange} name='Time' placeholder="시간"></input>
+        <input class="sub_input_text" value={this.state.Main_Events} onChange={this.handleChange} name='Main_Events' placeholder="주요 사건"></input>
+        <input class="sub_input_text" value={this.state.Material} onChange={this.handleChange} name='Material' placeholder="소재"></input>
+        <button class="start" onClick = {this.requestcontents}>Start!</button>
+      </div>
+
+      <div class="main">
+        {this.state.loading ? 
+        <div class="loading"> <Spinner size='8px' color='#3b2479'/> </div> : null}
+        <div>
           <div>
             <textarea class="output" value={this.state.output} readOnly></textarea>
             <div class="reset">
@@ -152,20 +110,10 @@ class Main extends Component {
                 <img  src={reseticon} class="reseticon"/>
               </a>
             </div>
-
-          {this.state.mainChar ? 
-          <div>
-            <div class="next_input">
-              <input class="next_text" value={this.state.next} onChange={this.handleChangeN} placeholder="다음 행동을 입력하세요!"></input>
-            </div>
-            <button class="next" onClick = {() => {this.requestNextContents(this.state.next)}}>Next!</button>
           </div>
-
-          : null}
-          
-        </div>
         </div>
       </div>
+      </Fragment>
     );
   }
 }
